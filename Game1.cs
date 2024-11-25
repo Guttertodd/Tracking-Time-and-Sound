167d8b40e3a343de4431098a536273fd64f46b45
339713273 <339713273@2260I-L29-026>
2024-11-25 09:41:11 -0500

    working

--- Game1.cs
+++ Game1.cs
@@ -13,12 +13,15 @@ namespace Tracking__Time__and_Sound
 
         Texture2D bombTexture;
         Rectangle bombRect;
-
+        Texture2D explosionTexture;
+        Rectangle explosionRect;
         SpriteFont timeFont;
 
         SoundEffect explode;
 
         float seconds;
+
+        bool exploded;
 
         
         public Game1()
@@ -32,8 +35,9 @@ namespace Tracking__Time__and_Sound
         {
             // TODO: Add your initialization logic here
 
+            exploded = false;
             base.Initialize();
-
+            explosionRect = new Rectangle(50, 50, 700, 400);
             bombRect = new Rectangle(50, 50, 700, 400);
             _graphics.PreferredBackBufferWidth = 800;
             _graphics.PreferredBackBufferHeight = 500;  
@@ -50,15 +54,20 @@ namespace Tracking__Time__and_Sound
             bombTexture = Content.Load<Texture2D>("bomb");
             timeFont = Content.Load<SpriteFont>("File");
             explode = Content.Load<SoundEffect>("explosion");
+            explosionTexture = Content.Load<Texture2D>("images");
+           
+          
+            
         }
 
         protected override void Update(GameTime gameTime)
         {
             seconds += (float)gameTime.ElapsedGameTime.TotalSeconds;
-            if (seconds > 10)
+            if (seconds > 15)
             {
                 seconds = 0f;
                 explode.Play();
+                exploded = true;
             }
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
@@ -76,8 +85,15 @@ namespace Tracking__Time__and_Sound
             _spriteBatch.Begin();
 
             _spriteBatch.Draw(bombTexture, bombRect, Color.White);
-            _spriteBatch.DrawString(timeFont, (10 - seconds).ToString("00.0"), new Vector2(270, 200), Color.Black);
+            _spriteBatch.DrawString(timeFont, (15 - seconds).ToString("00.0"), new Vector2(270, 200), Color.Black);
+            
+            if (exploded)
+                _spriteBatch.Draw(explosionTexture, explosionRect, Color.White);
+           
+            
 
+            
+            
             _spriteBatch.End();
             base.Draw(gameTime);
         }
